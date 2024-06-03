5b67f9ccaea98846527ad1ede28827c48691c076
Yaron Kaikov <yaronkaikov@scylladb.com>
2024-06-03 22:27:49 +0300

    wip

--- scripts/pull_github_pr.sh
+++ scripts/pull_github_pr.sh
@@ -73,7 +73,7 @@ closes="${NL}${NL}Closes ${PROJECT}#${PR_NUM}${NL}"
 
 if [[ $nr_commits == 1 ]]; then
   git merge --squash FETCH_HEAD
-  git commit --no-edit
+  git commit -m "Merge '$PR_TITLE' from $USER_NAME" -m "${PR_DESCR}${closes}"
 else
 	git merge --no-ff --log=1000 FETCH_HEAD -m "Merge '$PR_TITLE' from $USER_NAME" -m "${PR_DESCR}${closes}"
 fi
