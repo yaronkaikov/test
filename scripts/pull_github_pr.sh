f56533ed1cd7d4c70949c6b69e74a55af94e04fa
Yaron Kaikov <yaronkaikov@scylladb.com>
2024-06-04 11:00:34 +0300

    wip

--- scripts/pull_github_pr.sh
+++ scripts/pull_github_pr.sh
@@ -69,22 +69,23 @@ echo "$USER_NAME"
 git fetch "$REMOTE" pull/$PR_NUM/head
 
 nr_commits=$(git log --pretty=oneline HEAD..FETCH_HEAD | wc -l)
-
+echo "${nr_commits}"
 closes="${NL}${NL}Closes ${PROJECT}#${PR_NUM}${NL}"
-
+echo "${closes}"
 if [[ $nr_commits == 1 ]]; then
   git merge --squash --log=1000 FETCH_HEAD -m "Merge '$PR_TITLE' from $USER_NAME" -m "${PR_DESCR}${closes}"
+  git commit --no-edit
 else
 	git merge --no-ff --log=1000 FETCH_HEAD -m "Merge '$PR_TITLE' from $USER_NAME" -m "${PR_DESCR}${closes}"
 fi
 git commit --amend # for a manual double-check
 
 # Check PR tests status
-PR_HEAD_SHA=$(jq -r .head.sha <<< $PR_DATA)
-PR_TESTS_STATUS=$(curl -s "https://api.github.com/repos/$PROJECT/commits/$PR_HEAD_SHA/status" | jq -r .state)
-if [ "$PR_TESTS_STATUS" != "success" ]; then
-  ORANGE='\033[0;33m'
-  NC='\033[0m'
-  echo -e "${ORANGE}\nWARNING:${NC} Some of the tests that ran for this PR were not completed successfully,\n" \
-        "please make sure all tests are done successfully before merge this PR.\n"
-fi
+  #PR_HEAD_SHA=$(jq -r .head.sha <<< $PR_DATA)
+  #PR_TESTS_STATUS=$(curl -s "https://api.github.com/repos/$PROJECT/commits/$PR_HEAD_SHA/status" | jq -r .state)
+  #if [ "$PR_TESTS_STATUS" != "success" ]; then
+  #  ORANGE='\033[0;33m'
+  #  NC='\033[0m'
+  #  echo -e "${ORANGE}\nWARNING:${NC} Some of the tests that ran for this PR were not completed successfully,\n" \
+  #        "please make sure all tests are done successfully before merge this PR.\n"
+  #fi
